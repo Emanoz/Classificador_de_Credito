0a4ed13b14902cc64bf1be8bbe2bb44b6638e93d
Emanoz <emanuel.lourencon@hotmail.com>
2020-10-04 20:50:52 -0300

    pequenas mudanças

--- prjCredito/polls/models.py
+++ prjCredito/polls/models.py
@@ -23,7 +23,7 @@ class Cadastro(models.Model):
     caminho_rg = models.CharField(max_length=150)
     caminho_cpf = models.CharField(max_length=150)
     caminho_comprovante_renda = models.CharField(max_length=150)
-    data_cadastro = models.DateTimeField()
+    data_cadastro = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.cpf
@@ -31,9 +31,16 @@ class Cadastro(models.Model):
 class Usuario(models.Model):
     email = models.CharField(max_length=150)
     senha = models.CharField(max_length=10)
-    ultimo_acesso = models.DateTimeField()
-    data_cadastro = models.DateTimeField()
-    nivel_acesso = models.CharField(max_length=15)
+    ultimo_acesso = models.DateTimeField(auto_now=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    NIVEL_ACESSO = (
+        ('C', 'Cliente'),
+        ('O', 'Operador'),
+        ('S', 'Supervisor'),
+    )
+    nivel_acesso = models.CharField(max_length = 1,
+                                    choices = NIVEL_ACESSO,
+                                    default = 'C')
 
     def __str__(self):
         return self.email
@@ -42,8 +49,20 @@ class Ficha(models.Model):
     id_cadastro = models.ForeignKey(Cadastro, on_delete=models.CASCADE)
     id_cliente = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='Cliente')
     id_operador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='Operador')
-    status = models.CharField(max_length=15)
-    data_ficha = models.DateTimeField()
+    STATUS = (
+        ('P', 'Pendente'),
+        ('E', 'Em análise'),
+        ('A', 'Aprovado'),
+        ('R', 'Reprovado'),
+        ('F', 'Falta de Informações'),
+    )
+    status = models.CharField(max_length=15,
+                            choices = STATUS,
+                            default = 'P')
+    data_ficha = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-data_ficha',)
 
     def __str__(self):
         return str(self.id)
