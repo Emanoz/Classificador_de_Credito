e66db7e69552b370312e278994a0a0de1bbc2959
Emanoz <emanuel.lourencon@hotmail.com>
2020-10-18 18:18:24 -0300

    Static files, templates e view de exemplo criadas

--- prjCredito/polls/views.py
+++ prjCredito/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from . models import Cadastro
 # Create your views here.
+
+class Home(ListView):
+    model = Cadastro
+    template_name = 'polls/home.html'
